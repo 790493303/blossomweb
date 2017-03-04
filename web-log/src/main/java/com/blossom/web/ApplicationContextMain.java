eeb586377323726bf5ab32a54ec0cb792300dfed
blossom <blossom_zhang@163.com>
2017-03-04 16:48:35 +0800

    add web ui module for blossomweb project

--- web-log/src/main/java/com/blossom/web/ApplicationContextMain.java
+++ web-log/src/main/java/com/blossom/web/ApplicationContextMain.java
@@ -2,13 +2,20 @@ package com.blossom.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
  * @deprecated : 程序入口
  * Created by Blossom on 2017/3/1.
  */
 @SpringBootApplication
-public class ApplicationContextMain {
+public class ApplicationContextMain extends SpringBootServletInitializer{
+
+   /* @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+
+        return application.sources(ApplicationContextMain.class);
+    }*/
 
     /**
      * @description 项目启动方法
